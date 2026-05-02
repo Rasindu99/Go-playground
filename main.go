8ab9db24e7aa37a583b57601abeeb88c2ebe0eac
Rasindu99 <rasindu0823@gmail.com>
2026-05-03 00:52:57 +0530

    Practice: Loops

--- main.go
+++ main.go
@@ -3,38 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	name := "RGR"
-	completedLessons := 6
-	totalLessons := 10
-	dailyStudyHours := 1.5
-	isConsistent := true
-	hasBuiltMiniProject := false
 
-	remainingLessons := totalLessons - completedLessons
-	progressPercentage := float64(completedLessons) / float64(totalLessons) * 100
+	topics := []string{"Variables", "Data Types", "If Else", "Loops", "Functions"}
+	var topicCount int
 
-	fmt.Println("Go Learning Score")
-	fmt.Println("-----------------")
-	fmt.Println("Name:", name)
-	fmt.Println("Completed Lessons:", completedLessons)
-	fmt.Println("Total Lessons:", totalLessons)
-	fmt.Println("Remaining Lessons:", remainingLessons)
-	fmt.Printf("Progress: %.1f%%\n", progressPercentage)
-	fmt.Printf("Daily Study Hours: %.1f\n", dailyStudyHours)
-	fmt.Println("Built Mini Project:", hasBuiltMiniProject)
-	fmt.Println("Consistent:", isConsistent)
-
-	fmt.Println()
-	fmt.Println("Progress Message")
-	fmt.Println("----------------")
-
-	if completedLessons >= 5 && isConsistent && hasBuiltMiniProject {
-		fmt.Println("You are ready for the next milestone")
-	} else if completedLessons >= 5 && isConsistent {
-		fmt.Println("Good progress. Build a mini project before moving forward")
-	} else if completedLessons >= 5 {
-		fmt.Println("You completed enough lessons, but consistency is important")
-	} else {
-		fmt.Println("Complete more lessons first")
+	fmt.Println("Go Study Planner \n----------------")
+	for index, topic := range topics {
+		fmt.Printf("%d. %s\n", index+1, topic)
 	}
+
+	fmt.Println("\nGo Study Planner \n----------------")
+	for _, topic := range topics {
+		topicCount++
+		fmt.Printf("Studing: %s\n", topic)
+	}
+	fmt.Printf("\nTotal Topics: %d\n", topicCount)
 }
