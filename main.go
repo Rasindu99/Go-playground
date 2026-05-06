1e5a59058b388e2cd55900d51829a97c112145b2
Rasindu99 <rasindu0823@gmail.com>
2026-05-06 22:31:05 +0530

    Practice: Functions

--- main.go
+++ main.go
@@ -4,18 +4,41 @@ import "fmt"
 
 func main() {
 
-	topics := []string{"Variables", "Data Types", "If Else", "Loops", "Functions"}
-	var topicCount int
+	fmt.Printf("The sum of 1, 2, 3, 4 and 5 is: %d\n", sum([]int{1, 3, 5, 8, 100}))
 
-	fmt.Println("Go Study Planner \n----------------")
-	for index, topic := range topics {
-		fmt.Printf("%d. %s\n", index+1, topic)
+	fmt.Printf("subtraction of 5 and 3.41 is: %.2f \n", sub(5, 3.41))
+
+	fmt.Println("Status of course completion: ", getStatus(3))
+
+	remainingLessons, progressPercentage, progressMessage := calculateProgress(3, 5)
+	fmt.Println("Remaining lessons:", remainingLessons)
+	fmt.Println("Progress percentage:", progressPercentage)
+	fmt.Println("Progress message:", progressMessage)
+}
+
+func sum(numbers []int) int {
+	total := 0
+
+	for _, value := range numbers {
+		total = total + value
+	}
+	return total
+}
+
+func sub(num1 int, num2 float64) float64 {
+	return float64(num1) - num2
+}
+
+func getStatus(completedLessons int) string {
+	if completedLessons >= 5 {
+		return "Congratulations! You have completed the course."
 	}
 
-	fmt.Println("\nGo Study Planner \n----------------")
-	for _, topic := range topics {
-		topicCount++
-		fmt.Printf("Studing: %s\n", topic)
-	}
-	fmt.Printf("\nTotal Topics: %d\n", topicCount)
+	return "Keep going! You have more lessons to complete."
+}
+
+func calculateProgress(completedLessons int, totalLessons int) (int, int, string) {
+	remainingLessons := totalLessons - completedLessons
+	progressPercentage := completedLessons * 100 / totalLessons
+	return remainingLessons, progressPercentage, fmt.Sprintf("You have completed %d%% of the course.", progressPercentage)
 }
