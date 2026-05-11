4a8be7c4c4c0ce12823eedd7cb691151d070c49f
Rasindu99 <rasindu0823@gmail.com>
2026-05-12 02:22:42 +0530

    practice: struct

--- main.go
+++ main.go
@@ -2,36 +2,59 @@ package main
 
 import "fmt"
 
-func printSkills(skills []string) {
-	for index, skill := range skills {
-		fmt.Printf("%d. %s\n", index+1, skill)
-	}
+type Person struct {
+	Name      string
+	Age       int
+	Salary    float64
+	IsTopG    bool
+	IsLoveGod bool
+	Address   Address
+}
+
+type Address struct {
+	District string
+	City     string
 }
 
 func main() {
 
-	user := map[string]string{
-		"name":     "R G R",
-		"age":      "26",
-		"salary":   "1250$",
-		"attitude": "Never Ever Give Up",
+	persons := map[int]Person{
+		1: {Name: "R G R", Age: 26, Salary: 1150, IsTopG: true, IsLoveGod: true, Address: Address{District: "Central", City: "Metropolis"}},
+		2: {Name: "U J", Age: 25, Salary: 2300, IsTopG: false, IsLoveGod: false, Address: Address{District: "West", City: "Gotham"}},
 	}
 
-	user["hobby"] = "Gaming"
-	user["salary"] = "1370$"
-	delete(user, "hobby")
+	topG := persons[1]
+	topG.Salary = 1500
+	persons[1] = topG
 
-	fmt.Println(user)
+	// fmt.Println(persons[1])
 
-	for key, value := range user {
-		fmt.Printf("%s : %s\n", key, value)
+	// john := Person{
+	// 	IsTopG: true,
+	// }
+
+	trisky := Person{"R G R", 26, 1350, true, true, Address{"western", "piliyandala"}}
+
+	doe := trisky
+	doe.Name = "John Doe"
+
+	promoteEmployee(&trisky)
+	trisky.addAllowance(50)
+	fmt.Println(trisky.City)
+
+}
+
+func printProfile(profile map[string]string) {
+	for key, value := range profile {
+		fmt.Printf("%s: %s\n", key, value)
 	}
+}
 
-	value, exists := user["hobby"]
+func promoteEmployee(employee *Person) {
+	employee.Salary += 100
+}
 
-	if exists {
-		fmt.Println("Hobby:", value)
-	} else {
-		fmt.Println("Hobby key does not exist in the user map.")
-	}
+func (employee *Person) addAllowance(amount int) {
+	employee.Salary += float64(amount)
+	fmt.Printf("Salary after allowance: %.2f\n", employee.Salary)
 }
